77f0165a08072b1e31bdfc6c2b999b2a9912b91e
bostev <146454098+bostev@users.noreply.github.com>
2026-09-25 20:32:43 +0400

    create-python-lxc.sh

--- create-python-lxc.sh
+++ create-python-lxc.sh
@@ -1,4 +1,6 @@
-# 1. Получаем ID и жестко очищаем его от невидимых символов (оставляем только цифры)
+#!/bin/bash
+
+# 1. Получаем ID и жестко очищаем его от невидимых символов
 RAW_ID=$(pvesh get /cluster/nextid)
 CTID=\((echo "\)RAW_ID" | tr -dc '0-9')
 
@@ -8,7 +10,7 @@ pveam update >/dev/null
 TEMPLATE=$(pveam available -section system | grep "debian-12-standard" | awk '{print $2}' | head -n 1)
 pveam download local $TEMPLATE
 
-# 3. Создаем контейнер ОДНОЙ строкой (без переносов), чтобы консоль не ломала команду
+# 3. Создаем контейнер ОДНОЙ строкой
 echo "[Info] Создаем контейнер с ID $CTID..."
 pct create \(CTID local:vztmpl/\){TEMPLATE##*/} -hostname python-dev -ostype debian -memory 1024 -cores 2 -unprivileged 1 -net0 name=eth0,bridge=vmbr0,ip=dhcp
 
